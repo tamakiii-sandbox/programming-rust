7d09533812980c2cff76bc87163d3d7654646409
tamakiii <tamakiii@users.noreply.github.com>
2023-07-17 15:51:42 +0900

    impl replace

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::env;
 use std::fs;
 use text_colorizer::*;
@@ -27,7 +28,20 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, &data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(4);
+        }
+    };
+
+    match fs::write(&args.output, &replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
@@ -72,4 +86,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
