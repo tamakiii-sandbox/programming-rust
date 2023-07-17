f32e1124fe088581fa4b52afe384aae2e746f574
tamakiii <tamakiii@users.noreply.github.com>
2023-07-17 11:21:58 +0900

    impl render

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -6,6 +6,33 @@ fn main() {
     println!("Hello, world!");
 }
 
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// which holds one grayscale pixel per byte. the `upper_left` and `lower_right`
+/// arguments specify points on the complex plane corresponding to the upper-
+/// left and lower-right corners of the pixel buffer.
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+
+            // bounds.0 is width
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
 /// Given the row and column of a pixel in the output image, return the
 /// corresponding point on the complex plane.
 ///
