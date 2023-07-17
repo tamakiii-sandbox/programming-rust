b91c0425b6f68479c008ed1066be7c9518de03c9
tamakiii <tamakiii@users.noreply.github.com>
2023-07-17 15:43:35 +0900

    read from INPUT and write to OUTPUT

--- quickreplace/src/main.rs
+++ quickreplace/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use text_colorizer::*;
 
 // enable println! macro to output {:?}
@@ -12,6 +13,33 @@ struct Arguments {
 
 fn main() {
     let args = parse_args();
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file '{}': {:?}",
+                "Error".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(2);
+        }
+    };
+
+    match fs::write(&args.output, &data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file '{}': {:?}",
+                "Err".red().bold(),
+                args.filename,
+                e
+            );
+            std::process::exit(3);
+        }
+    };
+
     println!("{:?}", args);
 }
 
