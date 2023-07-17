9378cb2a6ad9ad437345decc12e6500fd1a9f8f5
tamakiii <tamakiii@users.noreply.github.com>
2023-07-17 11:48:54 +0900

    impl main

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,12 +1,31 @@
 use image::codecs::png::{self, PngEncoder};
 use image::{ColorType, GrayImage, ImageEncoder};
 use num::Complex;
+use std::env;
 use std::f64::INFINITY;
 use std::fs::File;
 use std::str::FromStr;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!(
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimentions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left coner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right coner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
 fn write_image(
