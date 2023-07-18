25b3676fa5f28af3999d4434a6a5e5e11b64247c
tamakiii <tamakiii@users.noreply.github.com>
2023-07-18 18:27:41 +0900

    add tests

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -142,4 +142,61 @@ mod tests {
         // and apply the sign inversion operator to it, resulting in `-4`.
         assert_eq!(-4_i32.abs(), -4);
     }
+
+    #[test]
+    fn test_checked_ops() {
+        // The sum of 10 an d20 can be represented as a u8.
+        assert_eq!(10_u8.checked_add(20), Some(30));
+
+        // Unfortunately, the sum of 100 and 200 cannot.
+        assert_eq!(100_u8.checked_add(200), None);
+
+        // Do the addition; ppanic if it overflows
+        // let x: u8 = 100;
+        // let y: u8 = 200;
+        // let sum = x.checked_add(y).unwrap();
+        // thread 'tests::test_checked_ops' panicked at 'called `Option::unwrap()` on a `None` value', src/main.rs:157:36
+
+        // Oddly, signed division can overflow too, in one particular case.
+        // A signed n-bit type can represent -2^{n-1}, but not 2^{n-1}.
+        assert_eq!((-128_i8).checked_div(-1), None);
+        assert_eq!((127_i8).checked_div(-1), Some(-127));
+    }
+
+    #[test]
+    fn test_wrapping_mul() {
+        // The first product can be represented as a u16;
+        // the second cannot, so we get 250000 module 2^{16}
+        assert_eq!(100_u16.wrapping_mul(200), 20000);
+        assert_eq!(500_u16.wrapping_mul(500), 53392);
+
+        // Operations on signed types may wrap to negative values.
+        assert_eq!(500_i16.wrapping_mul(500), -12144);
+
+        // In bitwise shift operations, the shift distance
+        // is wrapped to fall within the size of the value.
+        // So a shift of 17 bits in a 16-bit type is
+        // a shift of 1.
+        assert_eq!(5_i16.wrapping_shl(17), 10);
+        assert_eq!(5_i16.wrapping_shl(1), 10);
+        assert_eq!(5_i16.wrapping_shl(2), 20);
+        assert_eq!(5_i16.wrapping_shl(3), 40);
+    }
+
+    #[test]
+    fn test_saturating_add_sub() {
+        assert_eq!(32760_i16.saturating_add(10), 32767);
+        assert_eq!((-32760_i16).saturating_sub(10), -32768);
+    }
+
+    #[test]
+    fn test_overflowing_add_sub() {
+        assert_eq!(255_u8.overflowing_sub(2), (253, false));
+        assert_eq!(255_u8.overflowing_sub(3), (252, false));
+        assert_eq!(255_u8.overflowing_add(2), (1, true));
+        assert_eq!(255_u8.overflowing_add(3), (2, true));
+
+        // A shift of 17 bits is too large for `u16`, and 17 modulo 16 is 1.
+        assert_eq!(5_u16.overflowing_shl(17), (10, true));
+    }
 }
