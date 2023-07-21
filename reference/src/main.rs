a727790bddc6cd718dce08a4a0b77c4b8a771fb2
tamakiii <tamakiii@users.noreply.github.com>
2023-07-21 17:59:35 +0900

    init hashmap

--- reference/src/main.rs
+++ reference/src/main.rs
@@ -1,3 +1,44 @@
+use std::collections::HashMap;
+type Table = HashMap<String, Vec<String>>;
+
 fn main() {
-    println!("Hello, world!");
+    let mut table = Table::new();
+    table.insert(
+        "Gesualdo".to_string(),
+        vec![
+            "many_madrigals".to_string(),
+            "Tenebrae Responsoria".to_string(),
+        ],
+    );
+    table.insert(
+        "Caravaggio".to_string(),
+        vec![
+            "The musicians".to_string(),
+            "The Calling of St. Matthew".to_string(),
+        ],
+    );
+    table.insert(
+        "Cellini".to_string(),
+        vec![
+            "Perseus with the head of Medusa".to_string(),
+            "a salt cellar".to_string(),
+        ],
+    );
+
+    show(table);
+}
+
+fn show(table: Table) {
+    for (artist, works) in table {
+        println!("works by {}:", artist);
+        for work in works {
+            println!("  {}", work);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_collections_hashmap() {}
 }
