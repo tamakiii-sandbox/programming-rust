216360c1f79f852ba834279602d1d15cb72cace3
tamakiii <tamakiii@users.noreply.github.com>
2023-07-21 16:00:10 +0900

    print_padovan()

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    print_padovan();
+}
+
+fn print_padovan() {
+    let mut padovan = vec![1, 1, 1]; // allocated here
+    for i in 3..10 {
+        let next = padovan[i - 3] + padovan[i - 2];
+        padovan.push(next);
+    }
+    println!("P(1..10) = {:?}", padovan); // dropped here
+}
+
+#[cfg(test)]
+mod test {
+    use crate::print_padovan;
+
+    #[test]
+    fn test_padovan() {
+        print_padovan();
+    }
 }
